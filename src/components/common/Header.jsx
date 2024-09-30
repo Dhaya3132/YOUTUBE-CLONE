4f52079b104720ab168e61332f96c5f3c9befcbb
Dhaya3132 <dhayalanu103@gmail.com>
2024-09-30 22:31:54 +0530

    feat - Navbar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
+import SearchBar from "./Searchbar";
+import Button from "@mui/material/Button";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import logo from "../../assets/public/logo.svg";
+import { Typography } from "@mui/material";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar sx={{ backgroundColor: "#FFF", boxShadow: "none" }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton size="large" edge="start">
+            <MenuIcon />
+          </IconButton>
+          <Box>
+            <img src={logo} alt="Example" width="120" height="100" />
+          </Box>
+        </Box>
+        <Box>
+          <SearchBar />
+        </Box>
+        <Box>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#fff",
+              border: "1px solid #ddd",
+              color: "#111",
+              borderRadius: "50px",
+              boxShadow: "none",
+              display:'flex',
+              alignItems:'center',
+              '&:hover':{
+                boxShadow:'none',
+                backgroundColor:'#f5f5f5'
+              },
+              padding:"9px 15px"
+            }}
+          >
+            <AccountCircleIcon sx={{ size: "md", marginRight:'4px' }} />
+            <Typography variant="p" textTransform="none">Sign in</Typography>
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/common/Searchbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Box, TextField, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+
+const SearchBar = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 'auto',
+        width:'550px'
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          borderRadius: '50px',
+          padding: '2px 4px',
+          boxShadow: 'none',
+          border: '1px solid #ddd',
+          width:'100%'
+        }}
+      >
+        <TextField
+          variant="standard"
+          placeholder="Search"
+          InputProps={{
+            disableUnderline: true,
+            sx: { borderRadius: '50px', paddingLeft: '15px' }, 
+          }}
+          sx={{ flex: 1 }} 
+        />
+
+        <IconButton sx={{ padding: '10px' }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      
+      <IconButton
+        sx={{
+          marginLeft: '10px',
+          backgroundColor: '#f1f1f1',
+          '&:hover': { backgroundColor: '#e0e0e0' },
+          borderRadius: '50%',
+        }}
+      >
+        <MicIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default SearchBar;
